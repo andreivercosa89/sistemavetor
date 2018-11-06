a888430b10c4c4cb4f4820da218d926339bb923b
andreivercosa <aluno@MacBook-Pro-de-Aluno.local>
2018-11-06 19:08:09 -0400

    vetor simples

---
+++ sistemavetor/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace sistemavetor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vetor = { 1, 2, 3, 4, 5 };
+
+            for (int i = 0; i < vetor.Length;i++){
+                Console.WriteLine("\nPosicao: {0} Valor: {1}", i, vetor[i]);
+            }
+        }
+    }
+}
